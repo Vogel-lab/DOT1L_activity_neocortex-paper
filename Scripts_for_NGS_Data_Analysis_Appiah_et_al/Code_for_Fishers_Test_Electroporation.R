56437f873b4f762ea265ade0b08c98ed63cff46c
Bismark Appiah <56589002+Appiah-bis@users.noreply.github.com>
2022-01-19 20:56:56 +0100

    Add files via upload

---
+++ Scripts_for_NGS_Data_Analysis_Appiah_et_al/Code_for_Fishers_Test_Electroporation.R
@@ -0,0 +1,33 @@
+# Run the following script before this analysis: Code_for_Electroporation_Dataset.Rmd
+# Computing contribution of each condition (DMSO/EPZ)to proportion of cells in each cluster
+
+clust <- sce_hvg.seurat$seurat_clusters
+clust_pts <- data.frame(names=names(clust),cluster=clust)
+clust_pts$condition <- substr(clust_pts$names, start = 0, stop = 9)
+levels(clust_pts$cluster)
+table_clust_pts <- table(clust_pts$condition, clust_pts$cluster)
+
+# Perform Fishers test
+datalist <- list()
+data24h <- data.frame("CellsIn"=c(1,1), "CellsOut"=c(1,1))
+
+for (j in 1:dim(table_clust_pts)[2]) {
+  for (i in 1:dim(table_clust_pts)[1]) {
+    data24h$CellsIn[i] <- table_clust_pts[i,j]
+    data24h$CellsOut[i] <- (sum(table_clust_pts[i,]) - table_clust_pts[i,j])
+    datalist[[j]] <- data24h
+    rownames(datalist[[j]]) <- rownames(table_clust_pts)
+  }
+}
+
+fisher_results <- list()
+
+for (i in 1:length(datalist)) {
+  fisher_results[[i]] <- fisher.test(datalist[[i]])
+  
+}
+
+fisher_results
+#Export results from Fishers Exact test
+capture.output(fisher_results,file = "fisher_results_EUE14ALL_seurat")
+######
