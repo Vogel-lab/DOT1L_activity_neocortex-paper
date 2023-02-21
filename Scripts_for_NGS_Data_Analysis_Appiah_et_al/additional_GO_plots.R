7ec1a06d84297449b2776911cf4e1e7fd80b68d8
Bismark Appiah <56589002+Appiah-bis@users.noreply.github.com>
2023-02-21 08:53:34 +0100

    Update additional_GO_plots.R

--- Scripts_for_NGS_Data_Analysis_Appiah_et_al/additional_GO_plots.R
+++ Scripts_for_NGS_Data_Analysis_Appiah_et_al/additional_GO_plots.R
@@ -5,7 +5,7 @@ library(org.Mm.eg.db)
 library(enrichplot)
 
 
-# GO analysis using genes upregulated in TTS compared to BP only in DMSO condition
+# GO analysis using genes downregulated in TTS compared to BP only in DMSO condition
 
 # Load data file named Decreased_In_C3_comp_IP_DMSO_electroporated.csv for this analysis
 symbols_1 <- as.character(read.csv2("Decreased_In_C3_comp_IP_DMSO_electroporated.csv")[,1]) #TTS_vs_AP_DEG_UP.csv
@@ -23,7 +23,7 @@ dotplot(ego_1, showCategory=20, orderBy="GeneRatio",title = "TTS vs BP (DMSO) - 
 ############################################################
 
 
-
+# GO analysis using genes upregulated in TTS compared to BP only in DMSO condition
 # Load data file named Increased_In_C3_comp_IP_DMSO_electroporated.csv for this analysis
 symbols_1 <- as.character(read.csv2("Increased_In_C3_comp_IP_DMSO_electroporated.csv")[,1]) #TTS_vs_AP_DEG_UP.csv
 gene <- mapIds(org.Mm.eg.db, symbols_1, 'ENTREZID', 'SYMBOL')
@@ -39,10 +39,9 @@ dotplot(ego_1, showCategory=20, orderBy="GeneRatio",title = "TTS vs BP (DMSO) - 
 
 
 ###########################################################
-# Increased_In_C3_comp_IP_DMSO_electroporated.csv
 
 
-# GO analysis using genes upregulated in TTS compared to APs only in DMSO condition
+# GO analysis using genes downregulated in TTS compared to Neurons only in DMSO condition
 
 # Load data file named Decreased_In_C3_comp_Neu_DMSO_electroporated.csv for this analysis
 symbols_1 <- as.character(read.csv2("Decreased_In_C3_comp_Neu_DMSO_electroporated.csv")[,1]) #TTS_vs_AP_DEG_UP.csv
@@ -59,6 +58,7 @@ ego_1 <- enrichGO(gene          = gene,
 dotplot(ego_1, showCategory=20,orderBy="GeneRatio",title = "TTS vs Neurons (DMSO) - Decreased Genes")
 ##########################
 
+# GO analysis using genes upregulated in TTS compared to Neurons only in DMSO condition
 # Load data file named Increased_In_C3_comp_Neu_DMSO_electroporated.csv for this analysis
 symbols_1 <- as.character(read.csv2("Increased_In_C3_comp_Neu_DMSO_electroporated.csv")[,1]) #TTS_vs_AP_DEG_UP.csv
 gene <- mapIds(org.Mm.eg.db, symbols_1, 'ENTREZID', 'SYMBOL')
@@ -74,22 +74,3 @@ ego_1 <- enrichGO(gene          = gene,
 dotplot(ego_1, showCategory=20,orderBy="GeneRatio",title = "TTS vs Neurons (DMSO) - Increased Genes")
 ##########################
 
-
-
-
-# GO analysis using genes upregulated in TTS compared to APs only in DMSO condition
-
-# Load data file named TTS_vs_AP_DEG_UP.csv for this analysis
-symbols_1 <- as.character(read.csv2("Decreased_In_C3_comp_IP_DMSO_electroporated.csv")[,1]) #TTS_vs_AP_DEG_UP.csv
-gene <- mapIds(org.Mm.eg.db, symbols_1, 'ENTREZID', 'SYMBOL')
-
-ego_1 <- enrichGO(gene          = gene,
-                  OrgDb         = org.Mm.eg.db,
-                  ont           = "CC",
-                  pAdjustMethod = "BH",
-                  pvalueCutoff  = 0.05,
-                  readable      = TRUE)
-
-
-
-dotplot(ego_1, showCategory=20,orderBy="GeneRatio",title = "TTS vs AP (DMSO) - Increased Genes (GO: CC)")
